f56d96a15f6026a0b35dedda46bf092ff07e23e7
Neeraj3508 <gneeraj32595@gmail.com>
2021-03-18 20:46:51 +0530

    changed color scheme

--- app.js
+++ app.js
@@ -12,31 +12,43 @@ admin.initializeApp({
 });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static(__dirname + "/static"))
+app.use(express.static(__dirname + "/static"));
 const PORT = process.env.PORT || 3000;
 
+// app.all("*", (req, res, next) => {
+//   console.log("Checking all route");
+//   next();
+// });
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/static/index.html");
 });
 
 // Whenever a user is accessing restricted content that requires authentication.
-app.get("/profile", (req, res) => {
-  const sessionCookie = req.cookies.session || "";
-  admin
-    .auth()
-    .verifySessionCookie(sessionCookie, true /** checkRevoked */)
-    .then(() => {
-      res.sendFile(__dirname + "/profile.html");
-    })
-    .catch((error) => {
-      res.redirect("/");
-    });
-});
+// app.get("/profile", (req, res) => {
+//   const sessionCookie = req.cookies.session || "";
+//   // console.log(`sessionCookie = ${sessionCookie}`)
+  
+//   admin
+//     .auth()
+//     .verifySessionCookie(sessionCookie, true /** checkRevoked */)
+//     .then(() => {
+//       console.log("session verified")
+//     })
+//     .catch((error) => {
+//       res.redirect("/");
+//       // res.send(error)
+//       // console.log(error)
+//     });
+// });
+
+// app.get("/profile", (req, res) => {
+//   res.send("profile")
+// })
 
 app.post("/sessionLogin", (req, res) => {
   const idToken = req.body.idToken.toString();
   // console.log(`idtoken from  Node Server = ${idToken}`);
-  verifySession(req, res, idToken);
+  createSession(req, res, idToken);
 });
 
 app.get("/sessionLogout", (req, res) => {
@@ -61,9 +73,10 @@ const verifySession = (req, res, idToken) => {
       // Only process if the user just signed in in the last 5 minutes.
       if (new Date().getTime() / 1000 - decodedIdToken.auth_time < 5 * 60) {
         // Create session cookie and set it.
-        createSession(req, res, idToken);
         return;
       }
+      // A user that was not recently signed in is trying to set a session cookie.
+      // To guard against ID token theft, require re-authentication.
       res.status(401).send("Recent sign in required!");
     });
 };
@@ -71,7 +84,6 @@ const verifySession = (req, res, idToken) => {
 const createSession = (req, res, idToken) => {
   console.log("Starting to create User Session");
   const expiresIn = 60 * 60 * 24 * 5 * 1000;
-
   admin
     .auth()
     .createSessionCookie(idToken, { expiresIn })
@@ -83,6 +95,6 @@ const createSession = (req, res, idToken) => {
       res.end(JSON.stringify({ status: "success" }));
     })
     .catch((error) => {
-      res.status(401).send("UNAUTHORIZED REQUEST!");
+      console.log(error)
     });
 };
--- static/js/main.js
+++ static/js/main.js
@@ -1,133 +1,170 @@
 // jshint esversion:6
-
-var firebaseConfig = {
-  apiKey: "AIzaSyBwH4XL_cwjrMIE1KNZAgAyXfDiHuenIbI",
-  authDomain: "n2adevelopers.firebaseapp.com",
-  projectId: "n2adevelopers",
-  storageBucket: "n2adevelopers.appspot.com",
-  messagingSenderId: "159122432683",
-  appId: "1:159122432683:web:8c29b9214b30e2a8fb0dee",
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-var provider = new firebase.auth.GoogleAuthProvider();
 const googleButton = document.getElementById("googleButton");
 const googleButtonFixed = document.getElementById("googleButtonFixed");
 const signOutButton = document.getElementById("signOutButton");
 const signOutButtonFixed = document.getElementById("signOutButtonFixed");
 
-const googleSignIn = () => {
-  firebase
-    .auth()
-    .signInWithPopup(provider)
-    .then((result) => {
-      var credential = result.credential;
+// let value_or_null = Cookies.get();
+// console.log(`Cookie value is ${Object.values(value_or_null)}`)
 
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = credential.accessToken;
-      // The signed-in user info.
-      var user = result.user;
-      console.log(`credential = ${credential}`);
-      console.log(`token = ${token}`);
-      console.log(`user = ${user}`);
-      googleButton.classList.add("d-none");
-      signOutButton.classList.remove("d-none");
-      googleButtonFixed.classList.add("d-none");
-      signOutButtonFixed.classList.remove("d-none");
-      firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
-    //   const csrfToken = getCookie("CSRF-TOKEN");
+// if (localStorage.getItem("email") != null) {
+//   googleButton.classList.add("d-none");
+//   signOutButton.classList.remove("d-none");
+//   googleButtonFixed.classList.add("d-none");
+//   signOutButtonFixed.classList.remove("d-none");
+// } else {
+//   googleButton.classList.remove("d-none");
+//   signOutButton.classList.add("d-none");
+//   googleButtonFixed.classList.remove("d-none");
+//   signOutButtonFixed.classList.add("d-none");
+// }
+// var firebaseConfig = {
+//   apiKey: "AIzaSyBwH4XL_cwjrMIE1KNZAgAyXfDiHuenIbI",
+//   authDomain: "n2adevelopers.firebaseapp.com",
+//   projectId: "n2adevelopers",
+//   storageBucket: "n2adevelopers.appspot.com",
+//   messagingSenderId: "159122432683",
+//   appId: "1:159122432683:web:8c29b9214b30e2a8fb0dee",
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
 
-    //   const headers = new Headers({
-    //     "Content-Type": "application/json",
-    //     "X-CSRF-TOKEN": "Neeraj",
-    //   });
-      fetch("/sessionLogin", {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        //   "CSRF-Token": token,
-        },
-        body: JSON.stringify({ token}),
-      });
-    })
-    .catch((error) => {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      console.log(`errorCode = ${errorCode}`);
-      console.log(`errorMessage = ${errorMessage}`);
-      console.log(`email = ${email}`);
-      console.log(`credential = ${credential}`);
-      // ...
-    });
-};
+// var provider = new firebase.auth.GoogleAuthProvider();
 
-const googleSignOut = () => {
-  firebase
-    .auth()
-    .signOut()
-    .then(() => {
-      console.log("Signed Out Successfully");
-      googleButton.classList.remove("d-none");
-      signOutButton.classList.add("d-none");
-      googleButtonFixed.classList.remove("d-none");
-      signOutButtonFixed.classList.add("d-none");
-    })
-    .catch((error) => {
-      console.log(`error while signing out = ${error}`);
-    });
-};
 
-googleButton.addEventListener("click", () => {
-  googleSignIn();
-});
+// // FIREBASE METHODS
+// const googleSignIn = () => {
+//   firebase
+//     .auth()
+//     .signInWithPopup(provider)
+//     .then((result) => {
+//       var credential = result.credential;
 
-signOutButton.addEventListener("click", () => {
-  googleSignOut();
-});
-googleButtonFixed.addEventListener("click", () => {
-  googleSignIn();
-});
+//       // This gives you a Google Access Token. You can use it to access the Google API.
+//       var token = credential.accessToken;
+//       // The signed-in user info.
+//       var user = result.user;
+//       console.log(`credential = ${credential}`);
+//       console.log(`token = ${token}`);
+//       console.log(`user = ${user}`);
+//       googleButton.classList.add("d-none");
+//       signOutButton.classList.remove("d-none");
+//       googleButtonFixed.classList.add("d-none");
+//       signOutButtonFixed.classList.remove("d-none");
+//       firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
+//       getIdToken(user.displayName, user.email);
+//     })
+//     .catch((error) => {
+//       // Handle Errors here.
+//       var errorCode = error.code;
+//       var errorMessage = error.message;
+//       // The email of the user's account used.
+//       var email = error.email;
+//       // The firebase.auth.AuthCredential type that was used.
+//       var credential = error.credential;
+//       console.log(`errorCode = ${errorCode}`);
+//       console.log(`errorMessage = ${errorMessage}`);
+//       console.log(`email = ${email}`);
+//       console.log(`credential = ${credential}`);
+//       // ...
+//     });
+// };
 
-signOutButtonFixed.addEventListener("click", () => {
-  googleSignOut();
-});
+// const googleSignOut = () => {
+//   localStorage.removeItem("displayName");
+//   localStorage.removeItem("email");
+//   firebase
+//     .auth()
+//     .signOut()
+//     .then(() => {
+//       console.log("Signed Out Successfully");
+//       googleButton.classList.remove("d-none");
+//       signOutButton.classList.add("d-none");
+//       googleButtonFixed.classList.remove("d-none");
+//       signOutButtonFixed.classList.add("d-none");
+//     })
+//     .catch((error) => {
+//       console.log(`error while signing out = ${error}`);
+//     });
+//   fetch("/sessionLogout");
+// };
 
+// const getIdToken = (displayName, email) => {
+//   localStorage.setItem("displayName", displayName);
+//   localStorage.setItem("email", email);
+//   firebase
+//     .auth()
+//     .currentUser.getIdToken(/* forceRefresh */ true)
+//     .then(function (idToken) {
+//       // Send token to your backend via HTTPS
+//       console.log(`Current User Token  = ${idToken}`);
+//       // PErforming fetch request
+//       fetch("/sessionLogin", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ idToken: idToken }),
+//       })
+//         .then(() => console.log("fetch API successfull"))
+//         .catch((error) => console.log(error));
+//       // ...
+//     })
+//     .catch(function (error) {
+//       // Handle error
+//       console.log(error);
+//     });
+// };
+
+// // EVENT LISTENERS
+// googleButton.addEventListener("click", () => {
+//   googleSignIn();
+// });
+
+// signOutButton.addEventListener("click", () => {
+//   googleSignOut();
+// });
+// googleButtonFixed.addEventListener("click", () => {
+//   googleSignIn();
+// });
+
+// signOutButtonFixed.addEventListener("click", () => {
+//   googleSignOut();
+// });
+
+// FIXED NAVBAR METHODS
 const navbarFixed = document.getElementById("fixed-nav");
 const navbarToggler = document.getElementById("navbar-toggler");
 const navbarTogglerFixed = document.getElementById("navbar-toggler-fixed");
-const navbarSupportedContent = document.getElementById("navbarSupportedContent");
-const navbarSupportedContentFixed = document.getElementById("navbarSupportedContentFixed");
-
+const navbarSupportedContent = document.getElementById(
+  "navbarSupportedContent"
+);
+const navbarSupportedContentFixed = document.getElementById(
+  "navbarSupportedContentFixed"
+);
 
 var scrollObject = {};
 window.onscroll = getScrollPosition;
 
-function getScrollPosition(){
-    scrollObject = {
-       x: window.pageXOffset,
-       y: window.pageYOffset
+function getScrollPosition() {
+  scrollObject = {
+    x: window.pageXOffset,
+    y: window.pageYOffset,
+  };
+  // If you want to check distance
+  if (scrollObject.y > 200) {
+    // add class
+    navbarFixed.style.top = "0";
+    if (navbarSupportedContent.classList.contains("show")) {
+      navbarToggler.classList.add("collapsed");
+      navbarSupportedContent.classList.remove("show");
     }
-    // If you want to check distance
-    if(scrollObject.y > 200) {
-        // add class
-        navbarFixed.style.top = "0";
-        if(navbarSupportedContent.classList.contains("show")) {
-          navbarToggler.classList.add("collapsed");
-          navbarSupportedContent.classList.remove("show");
-        }
-    } else {
-        // remove class
-        if(navbarSupportedContentFixed.classList.contains("show")) {
-          navbarTogglerFixed.classList.add("collapsed");
-          navbarSupportedContentFixed.classList.remove("show");
-        }
-        navbarFixed.style.top = "-6rem";
+  } else {
+    // remove class
+    if (navbarSupportedContentFixed.classList.contains("show")) {
+      navbarTogglerFixed.classList.add("collapsed");
+      navbarSupportedContentFixed.classList.remove("show");
     }
+    navbarFixed.style.top = "-6rem";
+  }
 }
